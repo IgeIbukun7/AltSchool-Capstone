4caab5db6dfa7276af70030c2d6543a0354ae66c
IgeIbukun7 <jonathan4lyf@gmail.com>
2024-08-14 23:12:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AltSchool-MedHealth
+This is the capstone project for the Altschool Africa frontend track. It is a tool that is used to find, export, and share hospitals within a particular region so that people can have quick access to healthcare services.
---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './components/NavBar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Search from './pages/Search';
+import Contact from './pages/Contact';
+import Login from './pages/UserLogin';
+import Signup from './pages/UserSignup';
+import NewHealthCenter from './pages/NewHealthCenter';
+
+
+const App: React.FC = () =>  {
+  return (<BrowserRouter>
+    <main>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>}  />
+        <Route path='/about' element={<About/>}  />
+        <Route path='/contact' element={<Contact/>}  />
+        <Route path='/search' element={<Search/>}  />
+        <Route path='/login' element={<Login/>}  />
+        <Route path='/signup' element={<Signup/>}  />
+        <Route path='/profile' element={<NewHealthCenter/>}  />
+      </Routes>
+      <Footer />
+    </main>
+    </BrowserRouter>
+  );
+}
+
+export default App;
